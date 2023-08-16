9e9aa441515b16748e3abea27660a93740776e44
Nipun Wimalasooriya <nipunrukshitha531@gmail.com>
2023-08-16 19:41:09 +0530

    added js files separately

---
+++ js/grounds.js
@@ -0,0 +1,42 @@
+fetch('http://localhost:8080/location/list')
+  .then(response => response.json())
+  .then(data => {
+    console.log("Printing");
+    const gridData = data;
+    const gridContainer = document.getElementById('grid-container');
+    const searchInput = document.getElementById('search-input');
+
+    gridContainer.style.display = 'grid';
+    gridContainer.style.gridTemplateColumns = 'repeat(4, 1fr)';
+
+    function updateGrid() {
+      gridContainer.innerHTML = '';
+
+      const searchValue = searchInput.value.toLowerCase();
+
+      gridData.forEach(location => {
+        if (location.name.toLowerCase().includes(searchValue)) {
+          const gridItem = document.createElement('div');
+          gridItem.classList.add('grid-item');
+          gridItem.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${location.imageUrls[0]})`;
+
+          const nameElement = document.createElement('h3');
+          nameElement.textContent = location.name;
+
+          const descriptionElement = document.createElement('p');
+          descriptionElement.textContent = location.location;
+
+          gridItem.appendChild(nameElement);
+          gridItem.appendChild(descriptionElement);
+
+          gridContainer.appendChild(gridItem);
+        }
+      });
+    }
+
+    searchInput.addEventListener('input', updateGrid);
+    updateGrid();
+  })
+  .catch(error => {
+    console.error('API Error:', error);
+  });
