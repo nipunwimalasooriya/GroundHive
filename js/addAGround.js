4ad607a66056dcc8ae4aa0dfa812769ec1c888fe
Nipun Wimalasooriya <nipunrukshitha531@gmail.com>
2023-08-16 23:25:08 +0530

    add add ground post request

---
+++ js/addAGround.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector(".addAGround-form");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    const formData = new FormData(form);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:8080/location", true);
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+          // Successful response from the server
+          console.log("Form submitted successfully!");
+          // You can perform additional actions here if needed
+        } else {
+          // Error handling
+          console.error("Form submission failed:", xhr.statusText);
+        }
+      }
+    };
+
+    xhr.send(formData);
+  });
+});
